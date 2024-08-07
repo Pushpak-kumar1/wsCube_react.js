5321d3cdbff64ae6abc4b02e152585a426b96a44
Pushpak <pushpak.kumar0101@gmail.com>
2024-08-07 13:57:19 +0530

    three hours completed

--- 01basics/src/App.jsx
+++ 01basics/src/App.jsx
@@ -2,33 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, TabContainer } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
+  
+
+  let displayData = () => {
+    alert("Welcome to alerts")
+  }
+
+  let addData=(a,b) => {
+    console.log(a+b)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        className='bg-[red] p-[10px] mr-4' onClick={()=>addData(20, 25)}>
+        Add Data </button>
+
+     <button
+        className='bg-[red] p-[10px]' onClick={displayData}>
+        Click me </button>
     </>
+
   )
 }
 
