f1ca600576a695f467bcbae0938b74c5015c3b89
Pushpak <pushpak.kumar0101@gmail.com>
2024-08-14 16:40:02 +0530

    lottery game completed

---
+++ acfiftynine/src/TicketNum.jsx
@@ -0,0 +1,10 @@
+import "./Lottery.css";
+import React from 'react'
+
+export default function TicketNum({num}) {
+  return (
+    <>
+     <span className='TicketNum'> {num} </span>
+    </>
+  )
+}
